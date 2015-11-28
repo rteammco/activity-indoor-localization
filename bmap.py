4fb6d006ed545cd900665c83e2d425d0326d3be0
Richard Teammco <teammco@cs.utexas.edu>
2015-11-28 15:24:53 -0600

    More comments.

--- bmap.py
+++ bmap.py
@@ -2,13 +2,25 @@ from errlog import log_error
 
 
 class BuildingMap():
+  """This class contains the bitmap data for the building map.
+  """
 
+  # Region defintions by ID (this must be the same as in the file).
   NUMBER_OF_REGIONS = 3
   VOID_SPACE = 0
   HALLWAY = 1
   STAIRCASE = 2
 
   def __init__(self, map_file_name):
+    """Reads the given file and formats the data for the bitmap.
+
+    Args:
+      map_file_name: the name of the text file containing all of the bitmap
+          data. This file should be have newline-delimited rows and
+          comma-delimited columns. Each "pixel" in the original map image should
+          have a corresponding bitmap value to indicate what type of region it
+          is in this file.
+    """
     try:
       map_file = open(map_file_name, 'r')
       self._map_data = map_file.readlines()
@@ -28,17 +40,24 @@ class BuildingMap():
 
   def probability_of(self, x, y):
     """Returns the probability at region x, y.
+
+    Args:
+      x: the (int) pixel x-position (col).
+      y: the (int) pixel y-position (row).
+
+    Returns:
+      The probability of the region that this position is covering. If the
+      given coordinates are out of range of the map's dimensions, returns 0
+      as the probability.
     """
     if x < 0 or x >= self.num_cols or y < 0 or y >= self.num_rows:
       return 0
     region = self._map_data[y][x]
-#    prob = self._region_probs[region]
-#    if region == 0:
-#      print '@ region 0, prob = {}'.format(prob)
     return self._region_probs[region]
 
   def set_probabilities(self, prob_map):
     """Sets the probabilities of each region."""
+    # TODO
     pass
 
 
--- errlog.py
+++ errlog.py
@@ -2,6 +2,17 @@ import inspect
 
 
 def log_error(message = 'An error happened.', terminate = False):
+  """Prints the given message and error details.
+
+  This function will print the given error message, and also the information
+  about where that error occured, including the file name, the line, and the
+  calling function.
+
+  Args:
+    message: the message from the calling function to describe the problem.
+    terminate: (optional) set to True if the app should quit after this error
+        is encountered.
+  """
   callerframerecord = inspect.stack()[1]
   frame = callerframerecord[0]
   info = inspect.getframeinfo(frame)
@@ -14,6 +25,6 @@ def log_error(message = 'An error happened.', terminate = False):
 
 
 if __name__ == '__main__':
-  # Test the log_error function.
+  """Quick test to make sure the log_error function works."""
   log_error('Testing log_error function.')
   log_error('Testing log_error function.', True)
